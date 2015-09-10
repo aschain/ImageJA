2fa32484109ccdf54d99a6ef1d884d458622fa60
A Schain <aaronssjunk@aol.com>
2015-09-10 15:29:32 -0400

    Prefer .ijm startupmacros for easier editing/running

--- src/main/java/ij/Menus.java
+++ src/main/java/ij/Menus.java
@@ -1518,10 +1518,10 @@ public class Menus {
 			} catch (Exception e) {}
 			return;
 		}
-		String path = macrosPath + "StartupMacros.txt";
+		String path = macrosPath + "StartupMacros.ijm";
 		File f = new File(path);
 		if (!f.exists()) {
-			path = macrosPath + "StartupMacros.ijm";
+			path = macrosPath + "StartupMacros.txt";
 			f = new File(path);
 			if (!f.exists()) {
 				(new MacroInstaller()).installFromIJJar("/macros/StartupMacros.txt");
